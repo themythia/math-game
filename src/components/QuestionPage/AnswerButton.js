e10c8e78149a8c9451557ccbf8a1b6f4214f1366
Emir Al <mythia@windowslive.com>
2022-07-04 17:42:32 +0300

    feat: add color change on AnswerButtons depending on answer

--- src/components/QuestionPage/AnswerButton.js
+++ src/components/QuestionPage/AnswerButton.js
@@ -1,20 +1,29 @@
 import { ReactComponent as ButtonBorder } from '../../assets/answerButton.svg';
+import { ReactComponent as BlackButtonBorder } from '../../assets/answerButtonBlack.svg';
+import { ReactComponent as GreenButtonBorder } from '../../assets/answerButtonGreen.svg';
 import { useParams, useNavigate } from 'react-router-dom';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { StatsContext } from '../../contexts/StatsContext';
 import { ThemeContext } from '../../contexts/ThemeContext';
 
 const AnswerButton = ({ value, type, clicked, setClicked }) => {
   const { questionId } = useParams();
   const navigate = useNavigate();
-
   const { stats, dispatch } = useContext(StatsContext);
   const { setTheme } = useContext(ThemeContext);
   const question = stats.questions[questionId - 1];
   const answer = question.multipliers[0] * question.multipliers[1];
+  const [thisButton, setThisButton] = useState(false);
+
+  // reset clicked button on page change
+  useEffect(() => {
+    setThisButton(false);
+  }, [questionId]);
 
   const handleClick = () => {
     setClicked(true);
+    setThisButton(true);
+    // if right answer selected
     if (value === answer) {
       setTheme('green');
       dispatch({
@@ -25,9 +34,11 @@ const AnswerButton = ({ value, type, clicked, setClicked }) => {
       window.setTimeout(() => {
         setTheme('dark');
         if (questionId < 10) {
+          // if there are still questions to answer
           navigate(`/${Number(questionId) + 1}`);
           dispatch({ type: 'INCREASE_QUESTION' });
         } else {
+          // if on final question
           dispatch({
             type: 'UPDATE_TOTAL',
             score: stats.current.score + Math.ceil(Math.sqrt(answer)),
@@ -39,14 +50,17 @@ const AnswerButton = ({ value, type, clicked, setClicked }) => {
         }
       }, 3000);
     } else {
+      // if answer is wrong
       setTheme('red');
       dispatch({ type: 'ADD_FALSE', index: questionId - 1 });
       window.setTimeout(() => {
         setTheme('dark');
         if (questionId < 10) {
+          // if there are still questions to answer
           navigate(`/${Number(questionId) + 1}`);
           dispatch({ type: 'INCREASE_QUESTION' });
         } else {
+          // if on final question
           dispatch({
             type: 'UPDATE_TOTAL',
             score: stats.current.score,
@@ -74,7 +88,11 @@ const AnswerButton = ({ value, type, clicked, setClicked }) => {
       className={`h-[178px] relative ${position}`}
       onClick={handleClick}
     >
-      <ButtonBorder />
+      {thisButton && <BlackButtonBorder />}
+      {!clicked || (clicked && !thisButton && value !== answer) ? (
+        <ButtonBorder />
+      ) : null}
+      {clicked && !thisButton && value === answer && <GreenButtonBorder />}
       <p className='text-90 relative bottom-[75%]'>{value}</p>
     </button>
   );
