09c0ee5e37f989fc88b52bdb96220dd3b90885c2
Emir Al <mythia@windowslive.com>
2022-07-04 11:03:09 +0300

    feat: customize tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customRed: '#FF0000',
+        customGreen: '#00BF63',
+        customBg: '#2D2D2D',
+      },
+      fontFamily: {
+        handlee: ['Handlee'],
+      },
+      fontSize: {
+        48: ['48px', '64px'],
+        52: ['52px', '69px'],
+        64: ['64px', '85px'],
+        82: ['82px', '109px'],
+        90: ['90px', '112px'],
+        128: ['128px', '171px'],
+      },
+    },
   },
   plugins: [],
 };
