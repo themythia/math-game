423a0efac7e3442cb883730804bc358d6cfccea5
Emir Al <mythia@windowslive.com>
2022-07-04 11:21:27 +0300

    feat: add ThemeContext

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Final from './components/Final';
 import Question from './components/Question';
 import Start from './components/Start';
+import ThemeWrapper from './contexts/ThemeContext';
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Start />} />
-        <Route path='/:questionId' element={<Question />} />
-        <Route path='/final' element={<Final />} />
-      </Routes>
-    </Router>
+    <ThemeWrapper>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Start />} />
+          <Route path='/:questionId' element={<Question />} />
+          <Route path='/final' element={<Final />} />
+        </Routes>
+      </Router>
+    </ThemeWrapper>
   );
 };
 export default App;
---
+++ src/contexts/ThemeContext.js
@@ -0,0 +1,24 @@
+import { createContext, useState } from 'react';
+const ThemeContext = createContext();
+
+const ThemeWrapper = ({ children }) => {
+  const [theme, setTheme] = useState('dark');
+
+  return (
+    <ThemeContext.Provider value={setTheme}>
+      <div
+        className={`${
+          theme === 'dark'
+            ? 'bg-customBg'
+            : theme === 'red'
+            ? 'bg-customRed'
+            : theme === 'green' && 'bg-customGreen'
+        } w-screen min-h-screen text-white font-handlee`}
+      >
+        {children}
+      </div>
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeWrapper;
