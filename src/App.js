1a65813a913dcf4ea3aac25075f27208a0b902fc
Emir Al <mythia@windowslive.com>
2022-07-04 17:03:56 +0300

    feat: add NotSupported page

--- src/App.js
+++ src/App.js
@@ -1,36 +1,43 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Final from './components/FinalPage/Final';
+import NotSupported from './components/NotSupported';
 import Question from './components/QuestionPage/Question';
 import PrivateRoute from './components/shared/PrivateRoute';
 import Start from './components/StartPage/Start';
 import StatsWrapper from './contexts/StatsContext';
 import ThemeWrapper from './contexts/ThemeContext';
+import useWindowSize from './hooks/useWindowSize';
 const App = () => {
+  const { width } = useWindowSize();
   return (
     <ThemeWrapper>
-      <StatsWrapper>
-        <Router>
-          <Routes>
-            <Route path='/' element={<Start />} />
-            <Route
-              path='/:questionId'
-              element={
-                <PrivateRoute>
-                  <Question />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path='/final'
-              element={
-                <PrivateRoute>
-                  <Final />
-                </PrivateRoute>
-              }
-            />
-          </Routes>
-        </Router>
-      </StatsWrapper>
+      {width < 1024 ? (
+        <NotSupported />
+      ) : (
+        <StatsWrapper>
+          <Router>
+            <Routes>
+              <Route path='/' element={<Start />} />
+              <Route
+                path='/:questionId'
+                element={
+                  <PrivateRoute>
+                    <Question />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path='/final'
+                element={
+                  <PrivateRoute>
+                    <Final />
+                  </PrivateRoute>
+                }
+              />
+            </Routes>
+          </Router>
+        </StatsWrapper>
+      )}
     </ThemeWrapper>
   );
 };
---
+++ src/components/NotSupported.js
@@ -0,0 +1,10 @@
+const NotSupported = () => {
+  return (
+    <div className='flex flex-row justify-center items-center h-screen w-screen'>
+      <p className='text-52'>
+        Screens with width less than 1024 pixels are not supported.
+      </p>
+    </div>
+  );
+};
+export default NotSupported;
